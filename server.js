176ddac138f43be877564b3f22e2b440f20a1247
Ameet Jayawant <ameetj85@gmail.com>
2023-03-25 19:50:14 -0400

    ExpressJS, Nodemon, IdeasRouter

--- server.js
+++ server.js
@@ -2,30 +2,6 @@ const express = require('express');
 
 const PORT = 5000;
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2022-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older that your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRogers',
-    date: '2022-01-02',
-  },
-  {
-    id: 3,
-    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
-    tag: 'Software',
-    username: 'BruceBanner',
-    date: '2022-01-02',
-  },
-];
-
 const app = express();
 
 app.get('/', (req, res) => {
@@ -34,28 +10,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/api/ideas', (req, res) => {
-  res.json({
-    success: true,
-    data: ideas,
-  });
-});
-
-app.get('/api/ideas/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    res.status(404).json({
-      success: false,
-      error: 'Resource not found',
-    });
-  } else {
-    res.send({
-      success: true,
-      data: idea,
-    });
-  }
-});
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter); // middleware setup to route to ideas router
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
