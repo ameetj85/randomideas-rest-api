446d73d3188f536e28c02dd88d72e7296f25b721
Ameet Jayawant <ameetj85@gmail.com>
2023-03-25 21:25:18 -0400

    Mongoose Connect and Dotenv

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+require('dotenv').config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
